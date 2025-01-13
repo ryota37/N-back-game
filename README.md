2d58e7eedc6543b146643c04966527a8212d1269
ryota37 <ryota.nakahama@access-company.com>
2025-01-13 22:27:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# What is this code?
+
+- A prototype of n-back task game working in a comman-line interface(CUI)
+
+# How to use
+
+- Execute the command below.
+
+```
+$ g++ -std=c++17 cli_app.cpp
+& ./a.out
+```
+
+- Then folow the instructions shown on your console.
+
+## The example of the cosole output
+
+```
+5+1=?
+Press Enter: 
+2+0=?
+Press Enter: 
+8-5=?
+Enter your answer: 6
+The answer is: 6
+Correct!
+9-2=?
+Enter your answer: 2
+The answer is: 2
+Correct!
+7-9=?
+Enter your answer: 3
+The answer is: 3
+Correct!
+Enter your answer: 7
+The answer is: 7
+Correct!
+Enter your answer: -2
+The answer is: -2
+Correct!
+```
+
+# Future development plan
+
+- In the current implementation, answers of the question sometimes have minus value.
+  - But it's too difficult. I will fix it later.
+- In the current implementatoin, the number of questions and the number of n-back is fixed(5,2 for each).
+  - I will fix it so that you specifty the both value from command option.
---
+++ cli_app.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+#include <utility>
+
+std::vector<std::pair<std::string, int>> generate_questions(int number_of_questions, int number_of_operators) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> number_generator(0, 9);
+    std::uniform_int_distribution<> operator_generator(0, number_of_operators - 1);
+    int num1;
+    int num2;
+    int answer;
+    int arithmetic_operator_id;
+    std::string arithmetic_operator;
+    std::vector<std::pair<std::string, int>> questions;
+
+    for (int i = 0; i < number_of_questions; i++) {
+        // Generate two random numbers and an arithmetic operator
+        num1 = number_generator(gen);
+        num2 = number_generator(gen);
+
+        // Generate an arithmetic operator
+        // 0: +, 1: -, 2: *, 3: /
+        arithmetic_operator_id = operator_generator(gen);
+        switch (arithmetic_operator_id) {
+            case 0:
+                arithmetic_operator = "+";
+                answer = num1 + num2;
+                break;
+            case 1:
+                arithmetic_operator = "-";
+                answer = num1 - num2;
+                break;
+            case 2:
+                arithmetic_operator = "×";
+                answer = num1 * num2;
+                break;
+            case 3:
+                arithmetic_operator = "÷";
+                answer = num1 / num2;
+                break;
+        }
+
+        std::string question = std::to_string(num1) + arithmetic_operator + std::to_string(num2) + "=?";
+
+        questions.emplace_back(question, answer);
+    }
+    return questions;
+}
+
+void console_io(std::vector<std::pair<std::string, int>> questions, int number_of_n_back) {
+    int number_of_questions = questions.size();
+    for (int i = 0; i < number_of_questions+number_of_n_back; i++) {
+        if (i < number_of_n_back){
+            std::cout << questions[i].first << std::endl;
+            std::cout << "Press Enter: ";
+            std::cin.ignore();
+        } else if (i >= number_of_n_back && i < number_of_questions) {
+            std::cout << questions[i].first << std::endl;
+            std::cout << "Enter your answer: ";
+            int user_answer;
+            std::cin >> user_answer;
+            std::cout << "The answer is: " << questions[i-number_of_n_back].second << std::endl;
+            if (user_answer == questions[i-number_of_n_back].second) {
+                std::cout << "Correct!" << std::endl;
+            } else {
+                std::cout << "Incorrect!" << std::endl;
+            }
+        } else {
+            std::cout << "Enter your answer: ";
+            int user_answer;
+            std::cin >> user_answer;
+            std::cout << "The answer is: " << questions[i-number_of_n_back].second << std::endl;
+            if (user_answer == questions[i-number_of_n_back].second) {
+                std::cout << "Correct!" << std::endl;
+            } else {
+                std::cout << "Incorrect!" << std::endl;
+            }
+        }
+    }
+}
+
+int main() {
+
+    std::vector<std::pair<std::string, int>> questions = generate_questions(5, 2);
+
+    console_io(questions, 2);
+
+    // for (auto question : questions) {
+    //     std::cout << question.first << std::endl;
+    //     std::cout << "Enter your answer: ";
+    //     int user_answer;
+    //     std::cin >> user_answer;
+    //     std::cout << "The answer is: " << question.second << std::endl;
+    //     if (user_answer == question.second) {
+    //         std::cout << "Correct!" << std::endl;
+    //     } else {
+    //         std::cout << "Incorrect!" << std::endl;
+    //     }
+    // }
+
+    return 0;
+}
